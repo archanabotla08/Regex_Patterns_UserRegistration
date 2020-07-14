e6c6c87190368a4cb42a4f09ac49ee9a326ed787
Nisha271 <bukkenisha@gmail.com>
2020-07-14 22:30:20 +0530

    Created branch LastName and updated file for regex matching lastname

--- pattern_expression.sh
+++ pattern_expression.sh
@@ -1,8 +1,8 @@
 #!/bin/bash -x
 shopt -s extglob
-read -p "enter the first name: " firstname
+read -p "enter the last name: " lastname
 pattern="^[A|B|C|D|E|F|G|H|I|J|K|L|M|N|O|P|Q|R|S|T|U|V|W|X|Y|Z]{1}[a-z]{2,}"
-if [[ $firstname =~ $pattern ]]
+if [[ $lastname =~ $pattern ]]
 then
         echo "valid"
 else
