482c94adf9780b8d12416c312d722ba537e4a26c
Nisha271 <bukkenisha@gmail.com>
2020-07-14 23:32:13 +0530

    created brance password_PatternUC-1_number and updated file for regex for matching password contain 1 numeric

--- pattern_expression.sh
+++ pattern_expression.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -x
 shopt -s extglob
 read -p "enter the password: " password
-pattern="^[a-z]{7,}[A-Z]{1,}"
+pattern="^[a-z]{7,}[0-9]{1,}"
 if [[ $password =~ $pattern ]]
 then
         echo "valid"
