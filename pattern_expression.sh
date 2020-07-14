fd6382fd02ff1bc162405fe91a06c89e46b5c448
Nisha271 <bukkenisha@gmail.com>
2020-07-14 23:42:16 +0530

    created brance password_PatternUC-9 and updated file for regex for matching password

--- pattern_expression.sh
+++ pattern_expression.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -x
 shopt -s extglob
 read -p "enter the password: " password
-pattern="^[a-z]{7,}[!@#$%^&*_-+=.]{1,}"
+pattern="^([a-z]{1,}[A-Z]{1,}[0-9]{1,}[!@#$%^&*_-+=.]{1,}){8,}$"
 if [[ $password =~ $pattern ]]
 then
         echo "valid"
