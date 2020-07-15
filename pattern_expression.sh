c7c4f956baee5b19d8198b6da33f381d1c2406ff
Nisha271 <bukkenisha@gmail.com>
2020-07-15 08:03:33 +0530

    Updated with Regex pattern

--- pattern_expression.sh
+++ pattern_expression.sh
@@ -2,9 +2,9 @@
 shopt -s extglob
 read -p "enter the number: " number
 
-#pattern="^[0-9]{10}$|([9]{1}[1]{1}\\s([0-9]{10})$)"
+pattern="^[9]{1}[1]{1}[[:space:]][6-9]{1}[0-9]{9}$"
 
-if [[ $number =~ ^[9]{1}[1]{1}([^a-zA-Z]{0}\\s([0-9]{10})$) ]]
+if [[ $number =~ $pattern ]]
 then
         echo "match"
 else
