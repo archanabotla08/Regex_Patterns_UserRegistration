165e4e5c611f661398ffb6d0848393a12739831b
Nisha271 <bukkenisha@gmail.com>
2020-07-15 19:47:09 +0530

    [Archana] Refactored the code for Pattern expression

--- pattern_expression.sh
+++ pattern_expression.sh
@@ -1,64 +1,99 @@
 #!/bin/bash
 shopt -s extglob
+counter=0
 function firstName()
 {
-read -p "enter the first name: " firstname
-pattern="^[[:upper:]]{1}[[:lower:]]{2,}"
-if [[ $firstname =~ $pattern ]]
-then
-        echo "FirstName valid"
-else
-        echo "FirstName Invalid - Enter with /FirstCharacter Captial/"
-fi
+   valid=true
+   while [ $valid ]
+   do
+      read -p "enter the first name: " firstname
+      pattern="^[[:upper:]]{1}[[:lower:]]{2,}"
+      if [[ $firstname =~ $pattern ]]
+      then
+            echo "FirstName valid"
+            valid=false
+            ((counter++))
+            break
+      else
+            echo "FirstName Invalid - Enter with /FirstCharacter Captial/"
+      fi
+   done
 }
 function lastName()
 {
-read -p "enter the last name: " lastname
-pattern="^[[:upper:]]{1}[[:lower:]]{2,}"
-if [[ $lastname =~ $pattern ]]
-then
-        echo "LastName valid"
-else
-        echo "LastName Invalid - Enter with /FirstCharacter Captial/"
-fi
+
+   valid=true
+   while [ $valid ]
+   do
+      read -p "enter the last name: " lastname
+      pattern="^[[:upper:]]{1}[[:lower:]]{2,}"
+      if [[ $lastname =~ $pattern ]]
+      then
+            echo "LastName valid"
+            valid=false
+            ((counter++))
+            break
+      else
+            echo "LastName Invalid - Enter with /FirstCharacter Captial/"
+      fi
+   done
 }
 function email()
 {
-read -p "enter the email: " email
-pattern="^[a-zA-Z0-9]+([.+_-]{1}[a-zA-Z0-9]{3,}){0,2}\@[a-zA-Z0-9]+([.]{1,2}[a-z]{2,}){0,2}"
-
-if [[ $email =~ $pattern ]]
-then
-        echo "Email valid"
-else
-        echo "Email Invalid - Enter with /FirstCharacters followed by [special characters or numbers then @ with [alpha values] .[required characters]]/"
-fi
+   valid=true
+   while [ $valid ]
+   do
+      read -p "enter the email: " email
+      pattern="^[a-zA-Z0-9]+([.+_-]{1}[a-zA-Z0-9]{3,}){0,2}\@[a-zA-Z0-9]+([.]{1,2}[a-z]{2,}){0,2}"
+      if [[ $email =~ $pattern ]]
+      then
+            echo "Email valid"
+            valid=false
+            ((counter++))
+            break
+      else
+            echo "Email Invalid - Enter with /FirstCharacters followed by [special characters or numbers then @ with [alpha values] .[required characters]]/"
+      fi
+   done
 }
 function mobileNumber()
 {
-read -p "enter the mobile number: " mobileno
-pattern="^[9]{1}[1]{1}[[:space:]][6-9]{1}[0-9]{9}$"
-if [[ $mobileno =~ $pattern ]]
-then
-        echo "Mobile number valid"
-else
-        echo "Mobile Number Invalid - Enter/91 space followed by 10 digits/"
-fi
+   valid=true
+   while [ $valid ]
+   do
+      read -p "enter the mobile number: " mobileno
+      pattern="^[9]{1}[1]{1}[[:space:]][6-9]{1}[0-9]{9}$"
+      if [[ $mobileno =~ $pattern ]]
+      then
+            echo "Mobile number valid"
+            valid=false
+            ((counter++))
+            break
+      else
+            echo "Email Invalid - Enter with /FirstCharacters followed by [special characters or numbers then @ with [alpha values] .[required characters]]/"
+      fi
+   done
 }
 function password()
 {
-read -p "enter the password : " password
-pattern_validation_1="^[a-zA-Z0-9]{8}"
-pattern_validation_2="[[:upper:]]+"
-pattern_validation_3="[[:digit:]]+"
-pattern_validation_4="[A-Za-z0-9]*[@#$%^&*-_=+.][A-Za-z0-9]*$"
-
-if [[ $password =~ $pattern_validation_1 && $password =~ $pattern_validation_2 && $password =~ $pattern_validation_3 && $password =~ $pattern_validation_4 ]]
-then
-        echo "Password Valid"
-else
-        echo "Password Invalid - Enter /password with mininum 8 characters or digits and atleast 1 Captial and at least 1 digit and at least 1 special symbol/"
-fi
+   valid=true
+   while [ $valid ]
+   do
+      read -p "enter the password : " password
+      pattern_validation_1="^.{8}"
+      pattern_validation_2="[[:upper:]]+"
+      pattern_validation_3="[[:digit:]]+"
+      pattern_validation_4="[A-Za-z0-9]*[@#$%^&*-_=+.][A-Za-z0-9]*$"
+      if [[ $password =~ $pattern_validation_1 && $password =~ $pattern_validation_2 && $password =~ $pattern_validation_3  && $password =~ $pattern_validation_4 ]]
+      then
+            echo "Password Valid"
+            valid=false
+            ((counter++))
+            break
+      else
+            echo "Password Invalid - Enter /password with mininum 8 characters or digits and atleast 1 Captial, at least 1 digit  and at least 1 special symbol/"
+      fi
+   done
 }
 
 firstName
@@ -66,3 +101,4 @@ lastName
 email
 mobileNumber
 password
+
