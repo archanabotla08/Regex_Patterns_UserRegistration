d6927371d98b5c47e39bff11b2da67bca4310018
Nisha271 <bukkenisha@gmail.com>
2020-07-15 11:32:24 +0530

    Updated Code in expression file for Regex

--- pattern_expression.sh
+++ pattern_expression.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -x
 shopt -s extglob
 read -p "enter the first name: " firstname
-pattern="^[A|B|C|D|E|F|G|H|I|J|K|L|M|N|O|P|Q|R|S|T|U|V|W|X|Y|Z]{1}[a-z]{2,}"
+pattern="^[[:upper:]]{1}[[:lower:]]{2,}"
 if [[ $firstname =~ $pattern ]]
 then
         echo "valid"
