1f7c5bf7f47ee44d88df9312ec5806ffc44a1018
Nisha271 <bukkenisha@gmail.com>
2020-07-24 22:01:47 +0530

    [Archana]Refactored the code

--- pattern_expression.sh
+++ pattern_expression.sh
@@ -1,18 +1,16 @@
 #!/bin/bash
 shopt -s extglob
-counter=0
 function firstName()
 {
-   valid=true
-   while [ $valid ]
-   do
+      flag=0;
+      for ((;flag=1;))
+      do
       read -p "enter the first name: " firstname
       pattern="^[[:upper:]]{1}[[:lower:]]{2,}"
       if [[ $firstname =~ $pattern ]]
       then
             echo "FirstName valid"
-            valid=false
-            ((counter++))
+            flag=1
             break
       else
             echo "FirstName Invalid - Enter with /FirstCharacter Captial/"
@@ -22,16 +20,15 @@ function firstName()
 function lastName()
 {
 
-   valid=true
-   while [ $valid ]
+   flag=0;
+   for((;flag=1;))
    do
       read -p "enter the last name: " lastname
       pattern="^[[:upper:]]{1}[[:lower:]]{2,}"
       if [[ $lastname =~ $pattern ]]
       then
             echo "LastName valid"
-            valid=false
-            ((counter++))
+            flag=0;
             break
       else
             echo "LastName Invalid - Enter with /FirstCharacter Captial/"
@@ -40,16 +37,15 @@ function lastName()
 }
 function email()
 {
-   valid=true
-   while [ $valid ]
+   flag=0;
+   for((;flag=1;))
    do
       read -p "enter the email: " email
       pattern="^[a-zA-Z0-9]+([.+_-]{1}[a-zA-Z0-9]{3,}){0,2}\@[a-zA-Z0-9]+([.]{1,2}[a-z]{2,}){0,2}"
       if [[ $email =~ $pattern ]]
       then
             echo "Email valid"
-            valid=false
-            ((counter++))
+            flag=0;
             break
       else
             echo "Email Invalid - Enter with /FirstCharacters followed by [special characters or numbers then @ with [alpha values] .[required characters]]/"
@@ -58,16 +54,15 @@ function email()
 }
 function mobileNumber()
 {
-   valid=true
-   while [ $valid ]
+   flag=0;
+   for((;flag=1;))
    do
       read -p "enter the mobile number: " mobileno
       pattern="^[9]{1}[1]{1}[[:space:]][6-9]{1}[0-9]{9}$"
       if [[ $mobileno =~ $pattern ]]
       then
             echo "Mobile number valid"
-            valid=false
-            ((counter++))
+            flag=0;
             break
       else
             echo "Email Invalid - Enter with /FirstCharacters followed by [special characters or numbers then @ with [alpha values] .[required characters]]/"
@@ -76,8 +71,8 @@ function mobileNumber()
 }
 function password()
 {
-   valid=true
-   while [ $valid ]
+   flag=0;
+   for((;flag=1;))
    do
       read -p "enter the password : " password
       pattern_validation_1="^.{8}"
@@ -87,8 +82,7 @@ function password()
       if [[ $password =~ $pattern_validation_1 && $password =~ $pattern_validation_2 && $password =~ $pattern_validation_3  && $password =~ $pattern_validation_4 ]]
       then
             echo "Password Valid"
-            valid=false
-            ((counter++))
+            flag=0;
             break
       else
             echo "Password Invalid - Enter /password with mininum 8 characters or digits and atleast 1 Captial, at least 1 digit  and at least 1 special symbol/"
